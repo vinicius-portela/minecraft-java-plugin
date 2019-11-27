8543d539dabb0a1ed06fa98e28a3cc8e5fb1e1e6
vinicius-portela <vinicius.portela.stm@gmail.com>
2019-11-27 17:35:52 -0300

    Added Utils.java, Added Utils.Write(), Added Write File repeatly

--- src/me/viniciusportela/minecraftjavaplugin/Main.java
+++ src/me/viniciusportela/minecraftjavaplugin/Main.java
@@ -1,13 +1,21 @@
 package me.viniciusportela.minecraftjavaplugin;
 
-import com.earth2me.essentials.api.Economy;
+import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.api.UserDoesNotExistException;
+import net.ess3.api.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
 
 public class Main extends JavaPlugin {
 
     static Plugin plugin;
+    final int secondsDelay = 5;
 
     @Override
     public void onEnable() {
@@ -17,11 +25,33 @@ public class Main extends JavaPlugin {
         // Bukkit.getLogger()
         getLogger().info("Plugin Loaded");
 
-        try {
+        // Get list of players
+        //OfflinePlayer[] players = Bukkit.getOfflinePlayers();
+
+        //0 = Tick Initial Delay
+        //20L = 20 Ticks -> de delay
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+            @Override
+            public void run() {
+                /*for(OfflinePlayer item : players){
+                    getLogger().info(ChatColor.RED + "Player :" + item.getName());
+                    Utils.write(item.getName());
+                }*/
+                OfflinePlayer[] players = Bukkit.getOfflinePlayers();
+                try {
+                    Utils.write("./players.txt", players);
+                } catch (IOException | UserDoesNotExistException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 0, 20L * secondsDelay);
+
+        /*try {
             Economy.getMoneyExact("Viniccius007");
         } catch (UserDoesNotExistException e) {
-            e.printStackTrace();
-        }
+            getLogger().info("User Viniccius007 doesn't exists");
+            //e.printStackTrace();
+        }*/
 
         super.onEnable();
     }
---
+++ src/me/viniciusportela/minecraftjavaplugin/Utils.java
@@ -0,0 +1,24 @@
+package me.viniciusportela.minecraftjavaplugin;
+
+import com.earth2me.essentials.api.UserDoesNotExistException;
+import net.ess3.api.Economy;
+import org.bukkit.OfflinePlayer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Utils {
+    public static void write(String filename, OfflinePlayer[] player) throws IOException, UserDoesNotExistException {
+        BufferedWriter outputWriter = new BufferedWriter(new FileWriter(filename));
+        for (OfflinePlayer offlinePlayer : player) {
+            BigDecimal money = Economy.getMoneyExact(Objects.requireNonNull(offlinePlayer.getName()));
+            outputWriter.write(Objects.requireNonNull(offlinePlayer.getName()) + ": " + money.toString());
+            outputWriter.newLine();
+        }
+        outputWriter.flush();
+        outputWriter.close();
+    }
+}
